af394490e2e8567da6a90c7a185ec5b0f1802277
cresuta <60229891+cresuta@users.noreply.github.com>
2022-03-09 10:43:13 -0500

    initial code setup for beanvariety controller and repo

---
+++ js-app/main.js
@@ -0,0 +1,13 @@
+const url = "https://localhost:44317/api/beanvariety/";
+
+const button = document.querySelector("#run-button");
+button.addEventListener("click", () => {
+    getAllBeanVarieties()
+        .then(beanVarieties => {
+            console.log(beanVarieties);
+        })
+});
+
+function getAllBeanVarieties() {
+    return fetch(url).then(resp => resp.json());
+}
